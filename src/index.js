947b90f8018f866352b4ef4136b2b094b198d879
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2023-03-09 15:51:24 +0200

    chore: init project

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from './features/todos/todoSlice';
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer: {
+    todos: todoReducer
+  }
+})
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
