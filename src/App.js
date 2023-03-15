9cddf9095737863282ca444572845bdfe29be9c2
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2023-03-15 12:02:48 +0200

    chore: project initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-blue-800 flex items-center justify-center text-white w-screen text-8xl font-bold text-center">
+      React redux & redux toolkit
     </div>
   );
 }
